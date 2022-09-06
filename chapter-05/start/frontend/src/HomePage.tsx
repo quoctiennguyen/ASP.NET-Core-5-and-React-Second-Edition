421c3ebd41cf6b33162f4b952757b4d52edc646c
Thomas <thnguyen@paritycorp.com>
2022-09-06 10:09:04 +0700

    v1

--- chapter-05/start/frontend/src/HomePage.tsx
+++ chapter-05/start/frontend/src/HomePage.tsx
@@ -6,7 +6,7 @@ import { getUnansweredQuestions, QuestionData } from './QuestionsData';
 import { Page } from './Page';
 import { PageTitle } from './PageTitle';
 import { PrimaryButton } from './Styles';
-
+import { useNavigate } from 'react-router-dom';
 export const HomePage = () => {
   const [questions, setQuestions] = React.useState<QuestionData[]>([]);
   const [questionsLoading, setQuestionsLoading] = React.useState(true);
@@ -19,8 +19,11 @@ export const HomePage = () => {
     };
     doGetUnansweredQuestions();
   }, []);
+
+  const navigate = useNavigate();
   const handleAskQuestionClick = () => {
     console.log('TODO - move to the AskPage');
+    navigate('ask');
   };
 
   return (
