9f5d0c61bbf728457613536c8fac1cdd165d387f
Thomas <thnguyen@paritycorp.com>
2022-10-19 09:21:37 +0700

    learning updated

--- chapter-12/finish/frontend/src/AppSettings.ts
+++ chapter-12/finish/frontend/src/AppSettings.ts
@@ -3,8 +3,8 @@ export const server = 'http://localhost:17525';
 export const webAPIUrl = `${server}/api`;
 
 export const authSettings = {
-  domain: 'your-tenantid.auth0.com',
-  client_id: 'your-clientid',
+  domain: 'dev-bjvagtwe.us.auth0.com',
+  client_id: 'J1FUtqgGewRVBA9YubJcloMbZFv3hNKP',
   redirect_uri: window.location.origin + '/signin-callback',
   scope: 'openid profile QandAAPI email',
   audience: 'https://qanda',
